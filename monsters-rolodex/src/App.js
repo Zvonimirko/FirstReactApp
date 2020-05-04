9663c41e47ccf3530069399d87bea9926d4230a0
Zvonimir <44640961+Zvonimirko@users.noreply.github.com>
2020-05-04 15:00:21 +0200

    Dodana search-box komponenta

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -10,24 +11,31 @@ class App extends Component {
         monsters: [],
         searchField: ''
     };
-  }
+
+    // this.handleChange = this.handleChange.bind(this); --> use arrow functions as class methods to auto bind this
+  };
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ monsters: users}));
-  }
+  };
+
+   handleChange = (e) => {
+    this.setState({ searchField: e.target.value})
+  };
 
 render() {
   const { monsters, searchField } = this.state;
-  const filteredMonsters = monsters.filter(monster => 
+  const filteredMonsters = monsters.filter(monster =>
     monster.name.toLowerCase().includes(searchField.toLowerCase())
-    )
+    );
+
   return (
     <div className="App">
-    <input type='search' placeholder='search monsters'
-      onChange={e => 
-      this.setState({ searchField: e.target.value})}
+       <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
        />
     <CardList monsters={filteredMonsters}></CardList>
     </div>
--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -4,12 +4,8 @@ import { Card } from '../card/card.component';
 
 import './card-list.style.css';
 
-export const CardList = (props) => {
-return (<div className='card-list'>{
-  props.monsters.map(monster => ( 
-    <Card key={monster.id} 
-      monster={monster}
-    />))}
+export const CardList = props => {
+return (<div 
+    className='card-list'>
+    {props.monsters.map(monster => (<Card key={monster.id} monster={monster}/>))}
     </div>)};
-
-    
